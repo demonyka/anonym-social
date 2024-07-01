fd1a869ad08bdcb0021d947386adbda22b763cb1
demonyka <cweranit@gmail.com>
2024-07-01 03:36:59 +0300

    fix form scroll

--- resources/js/Pages/Auth/Forms/LoginForm.vue
+++ resources/js/Pages/Auth/Forms/LoginForm.vue
@@ -1,21 +1,50 @@
 <script>
 import PrimaryButton from "@/Elements/PrimaryButton.vue";
+import {useForm} from "@inertiajs/vue3";
 
 export default {
     name: "LoginForm",
-    components: {PrimaryButton}
+    components: {
+        PrimaryButton
+    },
+    data() {
+        return {
+            form: useForm({
+                login: '',
+                password: ''
+            })
+        }
+    },
+    methods: {
+        handleFocus(event) {
+            const element = event.target;
+            this.$nextTick(() => {
+                element.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                });
+            });
+        },
+        formSubmit() {
+
+        }
+    }
 }
 </script>
 
 <template>
-    <form class="auth">
+    <form class="auth" @submit.prevent="formSubmit">
         <h1>Вход</h1>
         <div class="inputs">
             <input
+                @focus="handleFocus"
+                v-model="form.login"
                 placeholder="Логин"
                 required
             >
             <input
+                @focus="handleFocus"
+                v-model="form.password"
                 placeholder="Пароль"
                 required
                 type="password"
