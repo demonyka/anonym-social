24fd47c9d2ae3bf17af104dc2820ac5904d1a52f
demonyka <cweranit@gmail.com>
2024-07-01 04:31:26 +0300

    fix content

--- resources/js/Layouts/Content.vue
+++ resources/js/Layouts/Content.vue
@@ -5,32 +5,9 @@ export default {
 </script>
 
 <template>
-    <div id="content">
-        <slot/>
-    </div>
+    <slot/>
 </template>
 
 <style scoped>
-    #content {
-        position: fixed;
-        width: 100dvw;
-        height: 100dvh;
-        left: 0;
-        top: 0;
-        overflow-x: hidden;
-        overflow-y: auto;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-    }
-    #content::-webkit-scrollbar {
-        width: 2px;
-    }
-    #content::-webkit-scrollbar-track {
-        background-color: var(--secondary-color);
-    }
-    #content::-webkit-scrollbar-thumb {
-        background-color: var(--bg-color);
-        border-radius: 10px;
-    }
+
 </style>
